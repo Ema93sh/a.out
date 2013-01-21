97173e5a370c9f687123d6bbac4915572526e386
vinith <vinith3773@gmail.com>
2013-01-21 17:11:34 +0530

    added solved problems to userprofile

--- frontend/online_judge/views.py
+++ frontend/online_judge/views.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from apps.practice.views import get_recent_activity
 from apps.practice.models import *
 from apps.submission.models import *
@@ -7,6 +9,20 @@ from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.contrib.auth import logout
 from django.template import RequestContext
 from django.contrib.auth.models import User
+
+def solvedProblems(user):
+   acuser=Submission.objects.filter(user=user,status = 'ACC')
+   solved=OrderedDict()
+   solved['practice']=[]
+   for s in acuser:
+      if s.contest:
+         solved[str(s.contest.code)]=[]
+         
+   for s in acuser:
+      pcode=str(s.problem.code)
+      ccode=str(s.contest.code) if s.contest else 'practice' 
+      if pcode not in solved[ccode]: solved[ccode].append(pcode)
+   return solved
 
 def home( request):
 	if request.user.is_authenticated():
@@ -21,7 +37,8 @@ def logoutView( request ):
 
 def profile( request, user_name ):
 	user = get_object_or_404( User, username=user_name )
-	return render_to_response('basic_profile.html', { 'required_user': user, 'recent_activity': get_recent_activity() }, context_instance=RequestContext(request) )
+	solved=solvedProblems(user)
+	return render_to_response('basic_profile.html', { 'required_user': user, 'solved_problems':solved ,'recent_activity': get_recent_activity() }, context_instance=RequestContext(request) )
 	
 def editprofile(request, user_name):
    user = get_object_or_404( User, username=user_name )
