dd6515360dd51e12685c6bd43a7e988ba47ee8fa
vinith <vinith3773@gmail.com>
2013-02-02 16:02:45 +0530

    shifted user related data to accounts app

--- frontend/apps/account/urls.py
+++ frontend/apps/account/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
-
+from views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
-		url(r'$','apps.account.views.registerUser'),		   
+               url(r'login$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+               url(r'(?P<user_name>[A-Za-z0-9-]+)/edit/$', 'apps.account.views.editprofile'),
+               url(r'(?P<user_name>[A-Za-z0-9-]+)/$', 'apps.account.views.profile'),
 		)
--- frontend/apps/account/views.py
+++ frontend/apps/account/views.py
@@ -4,6 +4,43 @@ from django.contrib.auth import logout
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from forms import *
+from apps.submission.models import *
+from collections import OrderedDict
+from apps.practice.views import get_recent_activity
+
+
+def solvedProblems(user):
+   acuser=Submission.objects.filter(user=user,status = 'ACC')
+   solved=OrderedDict()
+   solved['practice']=[]
+   for s in acuser:
+      if s.contest:
+         solved[str(s.contest.code)]=[]
+         
+   for s in acuser:
+      pcode=str(s.problem.code)
+      ccode=str(s.contest.code) if s.contest else 'practice' 
+      if pcode not in solved[ccode]: solved[ccode].append(pcode)
+   return solved
+
+def profile( request, user_name ):
+        user = get_object_or_404( User, username=user_name )
+        solved=solvedProblems(user)
+        return render_to_response('basic_profile.html', { 'required_user': user, 'solved_problems':solved ,'recent_activity': get_recent_activity() }, context_instance=RequestContext(request) )
+        
+def editprofile(request, user_name):
+   user = get_object_or_404( User, username=user_name )
+   if user == request.user:
+      if request.method == 'POST':
+         user.first_name = request.POST['first_name']
+         user.last_name = request.POST['last_name']
+         user.email = request.POST['email']
+         user.save()
+         return redirect( '/account/'+user.username )
+      return render_to_response('editable_profile.html', {'recent_activity': get_recent_activity() }, context_instance=RequestContext(request) )
+         
+   else:
+      return redirect( '/account/'+user.username )   
 
 def registerUser(request):
    msg=''
